fd577e4a68dab26ab2cac2d771211132cb54325b
Paul Wittchen <wittchenpaul@gmail.com>
2020-10-02 12:35:01 +0200

    Connected Storyboard to Swift

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -3,16 +3,35 @@
 //  TipCalculator
 //
 //  Created by Paul Wittchen on 02.10.20.
+// Copyright 2020 Paul Wittchen. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var amountPeopleTextField: UITextField!
+    
+    @IBOutlet weak var procentLabel: UILabel!
+    @IBOutlet weak var procentSlider: UISlider!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var amountPerPersonLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func procentSliderAction(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func procentButtonAction(_ sender: UIButton) {
+        
+    }
+    
 }
 
